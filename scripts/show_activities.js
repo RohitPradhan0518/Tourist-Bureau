d45ae28999913a22ce7541529bc92f152bdd3bfa
Rohitpradhan <Rohitpradhan0518@gmail.com>
2023-11-02 10:57:09 -0500

    drop down menu

--- scripts/show_activities.js
+++ scripts/show_activities.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    document.body.innerHTML += "Hello";
+   
+   
+    function select(id, list){
+        const e = document.createElement("select");
+        e.id = id;
+
+        const defaultOption = document.createElement("option")
+
+
+        for(let i = 0; i <list.length; i++){
+            const o = document.createElement("option");
+            o.innerText = list[i];
+            e.appendChild(o);
+        }
+        return e;
+    }
+
+    
+    document.body.appendChild(select("cateregories",  categories));
 })
