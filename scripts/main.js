3b10cd27f732beeae0fde2cffa616d82639d5b20
Rohitpradhan <Rohitpradhan0518@gmail.com>
2023-11-02 10:10:38 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    const nav = document.querySelector("nav"); 
+    nav.innerHTML = `
+    <a href="index.html"> Home </a>
+    <a href="activities.html"> Activities </a>
+    <a href="hikes.html"> Hikes </a>
+    <a href="video.html"> Video </a>
+    `;
+
+
+});
